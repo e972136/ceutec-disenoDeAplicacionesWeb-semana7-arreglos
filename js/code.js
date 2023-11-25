2faa2e610c2b6dc541801c4f5e20456a6a19ec02
Gaspar <e972136@gmail.com>
2023-11-25 09:57:48 -0600

    formateado

--- js/code.js
+++ js/code.js
@@ -4,23 +4,29 @@ let btnGuadrar = document.getElementById("btnGuadrar");
 let btnMostrar = document.getElementById("btnMostrar");
 let listaInfo = document.getElementById("listaInfo");
 const datos=[];
-console.log("x");
+
+
 function acumular(){
 //const person = {firstName:"John", lastName:"Doe", age:46};
     const x = {nombre:nombre.value,edad:edad.value};
     datos.push(x);
+    nombre.value = "";
+    edad.value = "";
 }
 
 function mostrar(){
+
     console.log("elementos");
     let html = "";
     /*    html = '<li class="list-group-item">'+operador.value + '\t' +  msg + '</li>';*/
     for(let i=0;i<datos.length;i++){
-        html+= '<li class="list-group-item">'+datos[i].nombre + '\t' +  datos[i].edad + '</li>';        
+        html+= '<li class="list-group-item">'+datos[i].nombre + ', ' +  datos[i].edad + '</li>';        
     }   
-    listaInfo.innerHTML = html;    
+    listaInfo.innerHTML = html;  
+    console.log(nombre.value+" "+edad.value);
 }
 
-
+/*
 btnGuadrar.addEventListener("click",acumular);
 btnMostrar.addEventListener("click",mostrar);
+*/
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 * [arreglo de objetos](https://www.w3schools.com/js/js_arrays.asp)
+* [manejo eventos boton](https://www.w3schools.com/jsref/event_onclick.asp)
